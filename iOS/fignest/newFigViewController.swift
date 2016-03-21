3992cd91a3c29d9061d12a29e18cd33c6466bc72
nlujan <naim.lujan@gmail.com>
2016-03-20 21:09:20 -0400

    added autocomplete feature

--- iOS/fignest/newFigViewController.swift
+++ iOS/fignest/newFigViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 import CoreLocation
 
-class newFigViewController: UIViewController, CLLocationManagerDelegate {
+class newFigViewController: UIViewController, CLLocationManagerDelegate, CLTokenInputViewDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    var names:[String] = []
+    var filteredNames:[String] = []
+    var selectedNames:[String] = []
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var tokenInputView: CLTokenInputView!
     
     @IBOutlet var locationTextField: UITextField!
     @IBOutlet var titleTextField: UITextField!
@@ -22,6 +28,7 @@ class newFigViewController: UIViewController, CLLocationManagerDelegate {
         print("\(locationTextField.text!)")
         print("\(titleTextField.text!)")
         print("\(foodTypeTextField.text!)")
+        print(selectedNames)
     }
     
     
@@ -36,9 +43,34 @@ class newFigViewController: UIViewController, CLLocationManagerDelegate {
             [NSForegroundColorAttributeName: UIColor.whiteColor()]
         
         
+//        self.tokenInputView.layer.borderWidth = 0.5
+//        self.tokenInputView.layer.borderColor = UIColor.grayColor().CGColor
+//        
+//        self.tokenInputView.layer.cornerRadius = 8.0
+        
+        
         // tapping outside screen clear keyboard
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-        view.addGestureRecognizer(tap)
+//        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+//        view.addGestureRecognizer(tap)
+        
+        
+        
+        //test autocomplete code
+        
+        names.appendContentsOf([
+            "Naim",
+            "Toks",
+            "Tom",
+            "Zach",
+            "Henry",
+            "Nikko Jo James"])
+        
+        self.tokenInputView.placeholderText = "Enter a name ";
+        self.tokenInputView.accessoryView = self.contactAddButton();
+        self.tokenInputView.drawBottomBorder = true;
+        self.tokenInputView.delegate = self
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         
     }
     
@@ -99,6 +131,7 @@ class newFigViewController: UIViewController, CLLocationManagerDelegate {
 
     }
     
+    
 
 
     /*
@@ -110,5 +143,134 @@ class newFigViewController: UIViewController, CLLocationManagerDelegate {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    
+    
+    
+    //MARK: CLTokenInputViewDelegate
+    
+    func tokenInputView(aView: CLTokenInputView, didChangeText text: String) {
+        // print("tokenInputView(didChangeText text:\(text))")
+        if text == "" {
+            self.filteredNames = []
+            self.tableView.hidden = true
+        }
+        else {
+            let predicate:NSPredicate = NSPredicate(format: "self contains[cd] %@", argumentArray: [text])
+            self.filteredNames = self.names.filter { predicate.evaluateWithObject($0) }
+            self.tableView.hidden = false
+        }
+        self.tableView.reloadData()
+    }
+    
+    func tokenInputView(aView:CLTokenInputView, didAddToken token:CLToken) {
+        self.selectedNames.append(token.displayText)
+    }
+    
+    func tokenInputView(aView:CLTokenInputView, didRemoveToken token:CLToken) {
+        let idx:Int? = self.selectedNames.indexOf(token.displayText)
+        self.selectedNames.removeAtIndex(idx!)
+    }
+    
+    func tokenInputView(aView: CLTokenInputView, tokenForText text: String) -> CLToken? {
+        //print("tokenInputView(tokenForText)")
+        if self.filteredNames.count > 0 {
+            let matchingName:String = self.filteredNames[0]
+            let match:CLToken = CLToken()
+            match.displayText = matchingName
+            match.context = nil
+            return match
+        }
+        return nil
+    }
+    
+    func tokenInputViewDidEndEditing(aView: CLTokenInputView) {
+        aView.accessoryView = nil
+    }
+    
+    func tokenInputViewDidBeginEditing(aView: CLTokenInputView) {
+        aView.accessoryView = self.contactAddButton()
+        self.view.layoutIfNeeded()
+    }
+    
+    func tokenInputView(aView:CLTokenInputView, didChangeHeightTo height:CGFloat) {
+        
+    }
+    
+    //MARK: UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //print("numberOfRowsInSection \(self.filteredNames.count)")
+        
+        return self.filteredNames.count
+    }
+    
+    
+    func tableView(aTableView: UITableView, cellForRowAtIndexPath anIndexPath: NSIndexPath) -> UITableViewCell {
+        //print("cellForRowAtIndexPath")
+        
+        let cell = aTableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: anIndexPath)
+        let name:String = self.filteredNames[anIndexPath.row]
+        cell.textLabel!.text = name
+        
+        //print("name = \(name)  cell=\(cell)")
+        if self.selectedNames.contains(name) {
+            cell.accessoryType = .Checkmark
+        }
+        else {
+            cell.accessoryType = .None
+        }
+        return cell
+    }
+    
+    //MARK: UITableViewDelegate
+    
+    func tableView(aTableView: UITableView, didSelectRowAtIndexPath anIndexPath: NSIndexPath) {
+        aTableView.deselectRowAtIndexPath(anIndexPath, animated: true)
+        let name:String = self.filteredNames[anIndexPath.row]
+        let token:CLToken = CLToken()
+        token.displayText = name
+        token.context = nil
+        if self.tokenInputView.isEditing() {
+            self.tokenInputView.addToken(token)
+        }
+    }
+    
+    //
+    
+    func onFieldInfoButtonTapped(sender:UIControl) {
+        let alertController = UIAlertController(title: "Field Info Button", message: "This view is optional and can be a UIButton, etc.", preferredStyle: .Alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            // ...
+        }
+        alertController.addAction(OKAction)
+        
+        self.presentViewController(alertController, animated: true) {
+            // ...
+        }
+    }
+    
+    func onAccessoryContactAddButtonTapped(sender:UIControl) {
+        let alertController = UIAlertController(title: "Accessory View Button", message: "This view is optional and can be a UIButton, etc.", preferredStyle: .Alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            // ...
+        }
+        alertController.addAction(OKAction)
+        
+        self.presentViewController(alertController, animated: true) {
+            // ...
+        }
+    }
+    
+    //
+    
+    func contactAddButton() -> UIButton {
+        let contactAddButton:UIButton = UIButton(type: .ContactAdd)
+        contactAddButton.addTarget(self, action: Selector("onAccessoryContactAddButtonTapped:"), forControlEvents: .TouchUpInside)
+        return contactAddButton
+    }
 
 }

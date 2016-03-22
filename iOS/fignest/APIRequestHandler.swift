251c6c814babea8eb989783c0371aa436b3ef8b6
nlujan <naim.lujan@gmail.com>
2016-03-22 19:58:30 -0400

    commit before playing with get and post functions

--- iOS/fignest/APIRequestHandler.swift
+++ iOS/fignest/APIRequestHandler.swift
@@ -12,7 +12,7 @@ class APIRequestHandler: NSObject {
     
     static let sharedInstance = APIRequestHandler()
     
-    let apiURL: String = "https://55cf99df.ngrok.io"
+    let apiURL: String = "https://9d460dd2.ngrok.io"
     
     var imagesURLs = [
         "https://s3-media4.fl.yelpcdn.com/bphoto/0OIgMcReW_hlcOQYNrKWjA/258s.jpg",

fa6b597cdf0647603165c2e34bfef8dd06eb3d85
Toks Fifo <toksfifo@gmail.com>
2016-03-22 19:19:37 -0400

    add yelp auth

--- api/yelp-api.js
+++ api/yelp-api.js
@@ -13,10 +13,10 @@ const start = '';
 const imgSelector = '[data-photo-id] .photo-box-img';
 const attribute = 'src';
 var yelp = new Yelp({
-  consumer_key: '',
-  consumer_secret: '',
-  token: '',
-  token_secret: ''
+  consumer_key: process.env.YELP_CONSUMER_KEY,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_CONSUMER_TOKEN,
+  token_secret: process.env.YELP_CONSUMER_SECRET
 });
 
 class YelpApi {

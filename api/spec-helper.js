aaeaaf23d170023f67ef77682e1f321b777601e3
Toks Fifo <toksfifo@gmail.com>
2016-07-23 23:46:03 -0400

    add spec helper to app spec

--- api/spec-helper.js
+++ api/spec-helper.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var _ = require('underscore');
+var ObjectId = require('mongodb').ObjectID;
+
 class SpecHelper {
   constructor() {
 
@@ -15,6 +18,10 @@ class SpecHelper {
     };
   }
 
+  getUsers(size) {
+    return _.map(_.range(size), (i) => this.getUser() );
+  }
+
   getEvent() {
     return {
       name: 'Sample Event 0',
@@ -25,6 +32,77 @@ class SpecHelper {
       search: 'bbq',
       isOver: false
     };
+  }
+
+  getEvents(size) {
+    return _.map(_.range(size), (i) => this.getEvent() );
+  }
+
+  getPlaceFromEventId(eventId) {
+    return _.extend(this.getPlace(), { event: eventId });
+  }
+
+  getActionFromEventId(eventId) {
+    return _.extend(this.getAction(), { event: eventId });
+  }
+
+  getActionFromEventIdAndWinningPlaceId(eventId, winningPlaceId) {
+    var action = _.extend(this.getAction(), { event: eventId });
+    action.selections[1].place = winningPlaceId;
+    return action;
+  }
+
+  // PRIVATE
+
+  getPlace() {
+    return { 
+      _id: new ObjectId(),
+      yelpId: 'graso-grill-new-york',
+      event: new ObjectId(),
+      name: 'Graso Grill',
+      rating: 4,
+      urls: {
+        web: 'http://www.yelp.com/biz/graso-grill-new-york?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=pluHcmRoKd3ypMFJ396lSA',
+        mobile: 'http://m.yelp.com/biz/graso-grill-new-york?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=pluHcmRoKd3ypMFJ396lSA' 
+      },
+      images: [
+        'https://s3-media1.fl.yelpcdn.com/bphoto/2o_is0mtfLBudBgPHGO51w/258s.jpg',
+        'https://s3-media4.fl.yelpcdn.com/bphoto/kvCDM0GJmorZ4paCpobN_A/258s.jpg',
+        'https://s3-media1.fl.yelpcdn.com/bphoto/ftOxs-HZo2n3JNhwwKErdA/258s.jpg',
+        'https://s3-media1.fl.yelpcdn.com/bphoto/W8PNTXUCp_1KwKeMptuCAA/258s.jpg',
+        'https://s3-media2.fl.yelpcdn.com/bphoto/9c5-a_qiQadpM0bFWrO3dQ/258s.jpg',
+        'https://s3-media1.fl.yelpcdn.com/bphoto/dIWFErYeanCIoNQr7Zb2tw/258s.jpg' 
+      ],
+      phone: '2127594848',
+      location: {
+        cross_streets: '5th Ave & Madison Ave',
+        city: 'New York',
+        geo_accuracy: 9.5,
+        postal_code: '10017',
+        country_code: 'US',
+        state_code: 'NY' 
+      }
+    }
+  }
+
+  getAction() {
+    return {
+      user: new ObjectId(),
+      event: new ObjectId(),
+      selections: [{
+        image: 'url0',
+        place: new ObjectId(),
+        isSelected: false
+      }, {
+        image: 'url1',
+        place: new ObjectId(),
+        isSelected: true
+      }, {
+        image: 'url2',
+        place: new ObjectId(),
+        isSelected: false
+      }]
+    }
   }
 }
 
--- api/spec/app-spec.js
+++ api/spec/app-spec.js
@@ -9,109 +9,8 @@ describe('API', () => {
   var db;
   var ObjectId = require('mongodb').ObjectID;
   var _ = require('underscore');
-
-  function _user0() {
-    return {
-      facebook: {
-        id: 'uniq0',
-        email: 'uniq0@gmail.com',
-        name: 'User0'
-      }
-    };
-  }
-
-  function _user1() {
-    return {
-      facebook: {
-        id: 'uniq1',
-        email: 'uniq10@gmail.com',
-        name: 'User1'
-      }
-    };
-  }
-
-  function _users() {
-    return [_user0(), _user1()];
-  }
-
-  function _event0() {
-    return {
-      name: 'Sample Event 0',
-      location: {
-        type: 'address',
-        address: 'East Village NYC'
-      },
-      search: 'bbq',
-      isOver: false
-    };
-  }
-
-  function _event1() {
-    return {
-      name: 'Sample Event 1',
-      location: {
-        type: 'address',
-        address: 'East Village NYC'
-      },
-      search: 'mexican',
-      isOver: false
-    };
-  }
-
-  function _events() {
-    return [_event0(), _event1()];
-  }
-
-  function _place(eventId) {
-    return { 
-      _id: new ObjectId(),
-      yelpId: 'graso-grill-new-york',
-      event: eventId,
-      name: 'Graso Grill',
-      rating: 4,
-      urls: {
-        web: 'http://www.yelp.com/biz/graso-grill-new-york?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=pluHcmRoKd3ypMFJ396lSA',
-        mobile: 'http://m.yelp.com/biz/graso-grill-new-york?utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=pluHcmRoKd3ypMFJ396lSA' 
-      },
-      images: [
-        'https://s3-media1.fl.yelpcdn.com/bphoto/2o_is0mtfLBudBgPHGO51w/258s.jpg',
-        'https://s3-media4.fl.yelpcdn.com/bphoto/kvCDM0GJmorZ4paCpobN_A/258s.jpg',
-        'https://s3-media1.fl.yelpcdn.com/bphoto/ftOxs-HZo2n3JNhwwKErdA/258s.jpg',
-        'https://s3-media1.fl.yelpcdn.com/bphoto/W8PNTXUCp_1KwKeMptuCAA/258s.jpg',
-        'https://s3-media2.fl.yelpcdn.com/bphoto/9c5-a_qiQadpM0bFWrO3dQ/258s.jpg',
-        'https://s3-media1.fl.yelpcdn.com/bphoto/dIWFErYeanCIoNQr7Zb2tw/258s.jpg' 
-      ],
-      phone: '2127594848',
-      location: {
-        cross_streets: '5th Ave & Madison Ave',
-        city: 'New York',
-        geo_accuracy: 9.5,
-        postal_code: '10017',
-        country_code: 'US',
-        state_code: 'NY' 
-      }
-    }
-  }
-
-  function _action(eventId, winningPlaceId) {
-    return {
-      user: new ObjectId(),
-      event: eventId,
-      selections: [{
-        image: 'url0',
-        place: new ObjectId(),
-        isSelected: false
-      }, {
-        image: 'url1',
-        place: winningPlaceId,
-        isSelected: true
-      }, {
-        image: 'url2',
-        place: new ObjectId(),
-        isSelected: false
-      }]
-    }
-  }
+  var SpecHelper = require('../spec-helper');
+  var helper = new SpecHelper();
 
   beforeAll((done) => {
     Mongo.connect().then((err) => {
@@ -145,8 +44,10 @@ describe('API', () => {
     });
 
     describe('when there are users', () => {
+      var users = helper.getUsers(2);
+
       beforeEach((done) => {
-        db.collection('users').insertMany(_users(), (err, res) => {
+        db.collection('users').insertMany(users, (err, res) => {
           done();
         });
       });
@@ -155,7 +56,7 @@ describe('API', () => {
         request.get(`${config.LOCAL_URL}/users`, (err, res, body) => {
           var response = JSON.parse(body);
           expect(_.isArray(response)).toBeTruthy();
-          expect(response.length).toBe(_users().length);
+          expect(response.length).toBe(users.length);
           done();
         });
       });
@@ -178,8 +79,10 @@ describe('API', () => {
 
   // GET /usersMapById
   describe('GET /usersMapById', () => {
+    var users = helper.getUsers(2);
+
     beforeEach((done) => {
-      db.collection('users').insertMany(_users(), (err, res) => {
+      db.collection('users').insertMany(users, (err, res) => {
         done();
       });
     });
@@ -187,7 +90,7 @@ describe('API', () => {
     it('returns a key for each user', (done) => {
       request.get(`${config.LOCAL_URL}/usersMapById`, (err, res, body) => {
         var response = JSON.parse(body);
-        expect(_.keys(response).length).toBe(_users().length);
+        expect(_.keys(response).length).toBe(users.length);
         done();
       });
     });
@@ -197,16 +100,17 @@ describe('API', () => {
 
   // POST /users
   describe('POST /users', () => {
+    var user0 = helper.getUser();
 
     it('responds with the posted user', (done) => {
       request.post({
         url: `${config.LOCAL_URL}/users`,
-        body: _user0(),
+        body: user0,
         json: true
       }, (err, res, body) => {
         expect(body._id).not.toBeNull();
-        expect(body.displayName).toBe(_user0().facebook.name);
-        expect(body.facebook.id).toBe(_user0().facebook.id);
+        expect(body.displayName).toBe(user0.facebook.name);
+        expect(body.facebook.id).toBe(user0.facebook.id);
         done();
       });
     });
@@ -214,7 +118,7 @@ describe('API', () => {
     it('adds the user to the db', (done) => {
       request.post({
         url: `${config.LOCAL_URL}/users`,
-        body: _user0(),
+        body: user0,
         json: true
       }, (err, res, body) => {
         db.collection('users').findOne({ _id: ObjectId(body._id) }, (err, res) => {
@@ -226,7 +130,7 @@ describe('API', () => {
 
     describe('if the user is already in the db', () => {
       beforeEach((done) => {
-        db.collection('users').insert(_user0(), (err, res) => {
+        db.collection('users').insert(user0, (err, res) => {
           done();
         });
       });
@@ -234,7 +138,7 @@ describe('API', () => {
       it('does NOT create a new user', (done) => {
         request.post({
           url: `${config.LOCAL_URL}/users`,
-          body: _user0(),
+          body: user0,
           json: true
         }, (err, res, body) => {
           var cursor = db.collection('users').find({ _id: ObjectId(body._id) });
@@ -254,8 +158,8 @@ describe('API', () => {
 
     describe('when the user has been invited to an event', () => {
       it('returns the invitation', (done) => {
-        var user = _user0();
-        var event = _event0();
+        var user = helper.getUser();
+        var event = helper.getEvent();
         db.collection('users').insert(user, (err, res) => {
           db.collection('events').insert(_.extend(event, {
             users: [user._id]
@@ -273,8 +177,8 @@ describe('API', () => {
 
     describe('when the user has been invited to multiple events', () => {
       it('returns multiple invitations', (done) => {
-        var user = _user0();
-        var events = _events();
+        var user = helper.getUser();
+        var events = helper.getEvents(2);
         db.collection('users').insert(user, (err, res) => {
           db.collection('events').insertMany(events.map((event) => _.extend(event, {
             users: [user._id]
@@ -291,7 +195,7 @@ describe('API', () => {
 
     describe('when the user has NOT been invited to any events', () => {
       it('does not return any invitations', (done) => {
-        var user = _user0();
+        var user = helper.getUser();
         db.collection('users').insert(user, (err, res) => {
           request.get(`${config.LOCAL_URL}/users/${user._id}/invitations`, (err, res, body) => {
             var response = JSON.parse(body);
@@ -310,7 +214,7 @@ describe('API', () => {
 
     describe('when the event exists', () => {
       it('returns the event', (done) => {
-        var event = _event0();
+        var event = helper.getEvent();
         db.collection('events').insert(event, (err, res) => {
           request.get(`${config.LOCAL_URL}/events/${event._id}`, (err, res, body) => {
             var response = JSON.parse(body);
@@ -338,16 +242,17 @@ describe('API', () => {
 
   // POST /events
   describe('POST /events', () => {
+    var event = helper.getEvent();
 
     it('responds with the posted event', (done) => {
       request.post({
         url: `${config.LOCAL_URL}/events`,
-        body: _event0(),
+        body: event,
         json: true
       }, (err, res, body) => {
         expect(body._id).not.toBeNull();
-        expect(body.name).toBe(_event0().name);
-        expect(body.search).toBe(_event0().search);
+        expect(body.name).toBe(event.name);
+        expect(body.search).toBe(event.search);
         expect(_.isNumber(body.location.radius)).toBeTruthy();
         done();
       });
@@ -356,7 +261,7 @@ describe('API', () => {
     it('adds the event to the db', (done) => {
       request.post({
         url: `${config.LOCAL_URL}/events`,
-        body: _event0(),
+        body: event,
         json: true
       }, (err, res, body) => {
         db.collection('events').findOne({ _id: ObjectId(body._id) }, (err, res) => {
@@ -375,7 +280,7 @@ describe('API', () => {
 
     describe('when the event does not have places yet', () => {
       it('creates and returns places', (done) => {
-        var event = _.extend(_event0(), { limit: Event.EVENT_LIMIT_DEFAULT });
+        var event = _.extend(helper.getEvent(), { limit: Event.EVENT_LIMIT_DEFAULT });
         db.collection('events').insert(event, () => {
           request.get(`${config.LOCAL_URL}/events/${event._id}/places`, (err, res, body) => {
             var response = JSON.parse(body);
@@ -392,7 +297,7 @@ describe('API', () => {
 
     describe('when the event already has places', () => {
       it('returns the current places', (done) => {
-        var event = _.extend(_event0(), { limit: Event.EVENT_LIMIT_DEFAULT });
+        var event = _.extend(helper.getEvent(), { limit: Event.EVENT_LIMIT_DEFAULT });
         db.collection('events').insert(event, () => {
           request.get(`${config.LOCAL_URL}/events/${event._id}/places`, (err, res, body) => {
             var response = JSON.parse(body);
@@ -418,7 +323,7 @@ describe('API', () => {
 
     describe('when there are no actions', () => {
       it('says there are no actions', (done) => {
-        var event = _event0();
+        var event = helper.getEvent();
         db.collection('events').insert(event, () => {
           request.get(`${config.LOCAL_URL}/events/${event._id}/solution`, (err, res, body) => {
             expect(body.indexOf('no actions') > -1).toBeTruthy();
@@ -430,13 +335,13 @@ describe('API', () => {
 
     describe('when there are actions', () => {
       beforeEach((done) => {
-        var event = _event0();
+        var event = helper.getEvent();
         db.collection('events').insert(event, () => {
           eventPersist = event;        
-          var place = _place(event._id);
+          var place = helper.getPlaceFromEventId(event._id);
           db.collection('places').insert(place, () => {
             placePersist = place;
-            var action = _action(event._id, place._id);
+            var action = helper.getActionFromEventIdAndWinningPlaceId(event._id, place._id);
             db.collection('actions').insert(action, () => {
               done();
             });
@@ -480,9 +385,9 @@ describe('API', () => {
   describe('POST /events/:eventid/actions', () => {
 
     it('responds with the posted action', (done) => {
-      var event = _event0();
+      var event = helper.getEvent();
       db.collection('events').insert(event, () => {
-        var action = _action(event._id);
+        var action = helper.getActionFromEventId(event._id);
         request.post({
           url: `${config.LOCAL_URL}/events/${event._id}/actions`,
           body: action,
@@ -498,9 +403,9 @@ describe('API', () => {
     });
 
     it('adds the action to the db', (done) => {
-      var event = _event0();
+      var event = helper.getEvent();
       db.collection('events').insert(event, () => {
-        var action = _action(event._id);
+        var action = helper.getActionFromEventId(event._id);
         request.post({
           url: `${config.LOCAL_URL}/events/${event._id}/actions`,
           body: action,

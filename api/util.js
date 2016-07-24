7ce1795460f594068ea2cae51ec85331c4ecc174
Toks Fifo <toksfifo@gmail.com>
2016-07-24 16:18:06 -0400

    ensure x places and y images per place

--- api/util.js
+++ api/util.js
@@ -1,9 +1,18 @@
 'use strict';
 
+var _ = require('underscore');
+
 class Util {
 	static milesToMeters(miles) {
 		return miles * 1609.344;
 	}
+
+  static ensureLength(collection, len) {
+    while (collection.length < len) {
+      collection.push(_.sample(collection));
+    }
+    return collection;
+  }
 }
 
 module.exports = Util;
